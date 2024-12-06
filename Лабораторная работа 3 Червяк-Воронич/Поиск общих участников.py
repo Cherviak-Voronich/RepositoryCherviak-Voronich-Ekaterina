0e5c603886f98334520feebedfdd9875c29cc2b0
Cherviak-Voronich <korolevamoa093@gmail.com>
2024-12-06 13:34:13 +0300

    Update Поиск общих участников.py

--- Лабораторная работа 3 Червяк-Воронич/Поиск общих участников.py
+++ Лабораторная работа 3 Червяк-Воронич/Поиск общих участников.py
@@ -1,9 +1,10 @@
 # TODO Напишите функцию find_common_participants
 def find_common_participants(first, second, rasd=","):
-    intersection_set = list(set(first.split(rasd)).intersection(set(second.split(rasd))))
+    first_group = set(first.split(rasd))    # Разделение первой группы
+    second_group = set(second.split(rasd))  # Разделение второй группы
+    intersection_set = list(first_group.intersection(second_group))     # Поиск пересечений между группами
     intersection_set.sort()
     return intersection_set
-
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
