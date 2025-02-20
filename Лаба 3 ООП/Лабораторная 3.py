c1de25e99ee13a2d2de1be7af7fff54ce8936968
Cherviak-Voronich <korolevamoa093@gmail.com>
2025-02-20 21:27:47 +0300

    Update Лабораторная 3.py

--- Лаба 3 ООП/Лабораторная 3.py
+++ Лаба 3 ООП/Лабораторная 3.py
@@ -1,27 +1,29 @@
 class Book:
     """ Базовый класс книги. """
     def __init__(self, name: str, author: str):
-        self._name = None   # делаем атрибут защищенным, добавив нижнее подчеркивание
-        self.set_name(name)    # устанавливаем с помощью метода название книги
-        self._author = None  # делаем атрибут защищенным, добавив нижнее подчеркивание
-        self.set_author(author)  # устанавливаем с помощью метода автора
+        self._name = name
+        self._author = author
 
     # Публичный метод, который внутри работает с защищенным атрибутом self._name
-    def get_name(self) -> int:
+    @property
+    def name(self) -> str:
         return self._name
 
     # Публичный метод, который внутри работает с защищенным атрибутом self._name
-    def set_name(self, name: str) -> None:
+    @name.setter
+    def name(self, name: str) -> None:
         if not isinstance(name, str):
             raise TypeError("Название должно быть типа str")
         self._name = name
 
     # Публичный метод, который внутри работает с защищенным атрибутом self._author
-    def get_author(self) -> int:
+    @property
+    def author(self) -> str:
         return self._author
 
     # Публичный метод, который внутри работает с защищенным атрибутом self._author
-    def set_author(self, author: str) -> None:
+    @author.setter
+    def author(self, author: str) -> None:
         if not isinstance(author, str):
             raise TypeError("Автор должен быть типа str")
         self._author = author
@@ -36,11 +38,11 @@ class Book:
 class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
         super().__init__(name, author)
-        self.pages = pages
+        self._pages = pages
 
     @property
     def pages(self) -> int:
-        return self.pages
+        return self._pages
 
     @pages.setter
     def pages(self, pages: int) -> None:
@@ -48,7 +50,7 @@ class PaperBook(Book):
             raise TypeError("Количество страниц должно быть типа int")
         if pages <= 0:
             raise ValueError("Количество страниц должно быть больше 0")
-        self.pages = pages
+        self._pages = pages
 
     def __str__(self):
         return f"Книга {self.name}. Автор {self.author}. Количество страниц {self.pages}"
@@ -60,19 +62,19 @@ class PaperBook(Book):
 class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
         super().__init__(name, author)
-        self.duration = duration
+        self._duration = duration
 
     @property
     def duration(self) -> int:
-        return self.duration
+        return self._duration
 
     @duration.setter
-    def duration(self, duration: int) -> None:
-        if not isinstance(duration, int):
-            raise TypeError("Продолжительность книги должна быть типа int")
+    def duration(self, duration: float) -> None:
+        if not isinstance(duration, (int, float)):
+            raise TypeError("Продолжительность книги должна быть типа int или float")
         if duration <= 0:
             raise ValueError("Продолжительность книги должна быть больше 0")
-        self.duration = duration
+        self._duration = float(duration)
 
     def __str__(self):
         return f"Книга {self.name}. Автор {self.author}. Продолжительность {self.duration}"
